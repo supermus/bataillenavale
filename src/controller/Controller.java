9d5c30d17d6408ed8d0fdecbc241efe8f31eded4
rgdev <rgdev@users.noreply.github.com>
2014-11-13 09:40:37 +0100

    Ajout Controller & MenuController

---
+++ src/controller/Controller.java
@@ -0,0 +1,8 @@
+package controller;
+
+import java.awt.event.ActionListener;
+
+public abstract class Controller implements ActionListener {
+
+
+}
--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -1,15 +1,19 @@
 package controller;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class MenuController implements ActionListener {
+public class MenuController extends Controller {
+	
+	public MenuController()
+	{
+		
+	}
 
-	// ICI sont reçus les évenements de l'interface du menu principal
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+
 
 }
