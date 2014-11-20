e977823d285dc19affa5a9d22205d59a74afeb47
mb2dev <bordage.mickael@gmail.com>
2014-11-20 09:18:25 +0100

    ajout de GameConfigView

--- src/controller/GameConfigController.java
+++ src/controller/GameConfigController.java
@@ -2,12 +2,31 @@ package controller;
 
 import java.awt.event.ActionEvent;
 
+import core.BatailleNavale;
+import view.GameConfigView;
+
 public class GameConfigController extends Controller{
 
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	private BatailleNavale batailleNavale;
+	
+	
+	public GameConfigController(){
 		
-	}
+			view = new GameConfigView();
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getSource() == ((GameConfigView)view).btnValider){
+				
+				
+			}
+			else if (e.getSource() == ((GameConfigView)view).btnAnnuler){
+				System.out.println("Retour au MENU");
+				batailleNavale.changeScreen(new MenuController(batailleNavale));
+			}
+			
+		}
 
 }
