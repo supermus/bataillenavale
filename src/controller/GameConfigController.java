b3e087e5d18b8fe98ba560a18428042a6dac0d66
rgdev <rgdev@users.noreply.github.com>
2014-12-11 11:41:35 +0100

    Nettoyage + support difficulté

--- src/controller/GameConfigController.java
+++ src/controller/GameConfigController.java
@@ -17,98 +17,83 @@ import view.GameConfigView;
 import core.BatailleNavale;
 
 public class GameConfigController extends AbstractController{
-	private ArrayList<Player>player = new ArrayList<Player>();
-	private Battle gameMode;
-	private Game game;
-	private Computer com;
-	private String speudo;
-	private String ModeJeux;
-	private int TailleMap;
-	private String Difficulter;
-	private String TypeBataille;
-	private int nbSousMarin;
-	private int nbPorteAvion;
-	private int nbCuirassésFurtif;
-	private int nbZodiac;
 	
 	
-	public GameConfigController(){
-			view = new GameConfigView(this);
-		}
+	public GameConfigController()
+	{
+		view = new GameConfigView(this);
+	}
 	
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			if(e.getSource() == ((GameConfigView)view).btnValider){
-				System.out.println("Début d'une partie");
-				speudo =((GameConfigView)view).txtUsername.getText();
-				System.out.println(speudo);
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		GameConfigView configView = (GameConfigView) view;
+		String playerName, gameType, difficulty, battleType;
+		int mapSize, amountBoat1, amountBoat2, amountBoat3, amountBoat4;
 		
-				//player = new Human(speudo);
-				
-				
-				// Nombre de joeur de la partie
-				ModeJeux=((GameConfigView)view).bg0.getSelection().getActionCommand();
-				System.out.println(ModeJeux);
-				
-				//Taille de la map
-				TailleMap=Integer.parseInt(((GameConfigView)view).bg1.getSelection().getActionCommand());
-				System.out.println(TailleMap);
-				
-				//Dificulter de la partie
-				Difficulter=((GameConfigView)view).bg2.getSelection().getActionCommand();
-				System.out.println(Difficulter);
-				
-				//Dificulter de la partie
-				TypeBataille=((GameConfigView)view).bg3.getSelection().getActionCommand();
-				System.out.println(TypeBataille);
-				
-				// Récupération des spinner
-				nbSousMarin = (Integer) ((GameConfigView)view).spinnerSousMarin.getValue();
-				System.out.println("Nombre de Sous-Marin : "+nbSousMarin);
-				nbPorteAvion = (Integer) ((GameConfigView)view).spinnerPorteAvion.getValue();
-				System.out.println("Nombre de porte Avion :" +nbPorteAvion);
-				nbCuirassésFurtif = (Integer) ((GameConfigView)view).spinnerCuirassésFurtif.getValue();
-				System.out.println("Nombre de Cuirassés Furtif : "+nbCuirassésFurtif);
-				nbZodiac = (Integer) ((GameConfigView)view).spinnerZodiac.getValue();
-				System.out.println("Nombre de Zodiac : "+nbZodiac);
-				if(TypeBataille.equals("BatailleNavalle"))
-					gameMode = new Classic();
-				if(TypeBataille.equals("AlerteRouge"))
-					gameMode = new RedAlert();
-				if(TypeBataille.equals("Artillerie"))
-					gameMode = new Artillery();
-				if(TypeBataille.equals("Radar"))
-					gameMode = new Radar();
-				
-				switch (ModeJeux){
-				case "Demo" : player.add(new Computer("Computer1", new Grid(TailleMap)));
-							  player.add(new Computer("Computer2", new Grid(TailleMap)));
-							  game = new Game(player,gameMode,TailleMap,nbSousMarin,nbPorteAvion,nbCuirassésFurtif,nbZodiac);
-							  break;
-				
-				case "1 Joueur" : player.add(new Computer("Computer1", new Grid(TailleMap)));
-				  				  player.add(new Human(speudo, new Grid(TailleMap)));
-				  				  game = new Game(player,gameMode,TailleMap,nbSousMarin,nbPorteAvion,nbCuirassésFurtif,nbZodiac);
-				  				  break;
-				}
-				
-				BatailleNavale.changeScreen(new GameController(game));
-//				if(ModeJeux == "Demo"){
-//					player.add(new Computer("Computer1"));
-//					player.add(new Computer("Computer2"));
-//				    gameMode = (GameMode) (Object)TypeBataille;
-//					game = new Game(player,gameMode,TailleMap);
-//					
-//				}
-				
-				}
-			else if (e.getSource() == ((GameConfigView)view).btnAnnuler){
-				System.out.println("Retour au MENU");
-				BatailleNavale.changeScreen(new MenuController());
+		if(e.getSource() == configView.btnValider)
+		{
+			// extraction des valeurs de configuration
+			playerName = configView.txtUsername.getText();	
+			gameType = configView.bg0.getSelection().getActionCommand();
+			mapSize = Integer.parseInt(configView.bg1.getSelection().getActionCommand());
+			difficulty = configView.bg2.getSelection().getActionCommand();
+			battleType = configView.bg3.getSelection().getActionCommand();
+			amountBoat1 = (int) configView.spinnerSousMarin.getValue();
+			amountBoat2 = (int) configView.spinnerPorteAvion.getValue();
+			amountBoat3 = (int) configView.spinnerCuirassésFurtif.getValue();
+			amountBoat4 = (int) configView.spinnerZodiac.getValue();
+			
+			// préparation de la partie
+			Battle battle = null;
+			ArrayList<Player> players = new ArrayList<Player>();
+			int computerDifficulty = Computer.DIFFICULTY_EASY;
+			Game game = null;
+
+			// type de bataille
+			if(battleType.equals("BatailleNavalle"))
+				battle = new Classic();
+			if(battleType.equals("AlerteRouge"))
+				battle = new RedAlert();
+			if(battleType.equals("Artillerie"))
+				battle = new Artillery();
+			if(battleType.equals("Radar"))
+				battle = new Radar();
+			
+			// niveau ordinateurs
+			if(difficulty.equals("Facile"))
+				computerDifficulty = Computer.DIFFICULTY_EASY;
+			else if(difficulty.equals("Intermédiaire"))
+				computerDifficulty = Computer.DIFFICULTY_NORMAL;
+			else
+				computerDifficulty = Computer.DIFFICULTY_HARD;
+			
+			// type de partie
+			if(gameType.equals("Demo"))
+			{
+				players.add(new Computer("Computer1", new Grid(mapSize), computerDifficulty));
+				players.add(new Computer("Computer2", new Grid(mapSize), computerDifficulty));
+			}
+			else if(gameType.equalsIgnoreCase("1 Joueur"))
+			{
+				players.add(new Human(playerName, new Grid(mapSize)));
+				players.add(new Computer("Computer2", new Grid(mapSize), computerDifficulty));
 			}
 			
+			System.out.println("Préparation d'une nouvelle partie");
+			System.out.println("type : " + battleType + " (" + gameType + ")");
+			System.out.println("map de taille " + mapSize + "x" + mapSize);
+			System.out.println("limites : " + amountBoat1 + " sous-marins, " + amountBoat2 + " porteavions, " + amountBoat3 + " cuirassés, " + amountBoat4 + " zodiacs.");
+			
+			game = new Game(players, battle, mapSize, amountBoat1, amountBoat2, amountBoat3, amountBoat4);
+			BatailleNavale.changeScreen(new GameController(game)); // passage à GameController
 		}
+		else if (e.getSource() == configView.btnAnnuler)
+		{
+				BatailleNavale.changeScreen(new MenuController()); // retour menu
+		}
+			
+	} // fin actionPerformed
 
 }
