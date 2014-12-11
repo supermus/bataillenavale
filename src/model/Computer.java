462348f69b05194e7f5b7fc04dd88d3d9e427fe7
rgdev <rgdev@users.noreply.github.com>
2014-12-11 11:41:48 +0100

    ajout difficulté

--- src/model/Computer.java
+++ src/model/Computer.java
@@ -1,10 +1,16 @@
 package model;
 
 public class Computer extends Player {
+	
+	public static int DIFFICULTY_EASY = 1;
+	public static int DIFFICULTY_NORMAL = 2;
+	public static int DIFFICULTY_HARD = 3;
+	
+	private int difficulty;
 
-	public Computer(String nom, Grid g) {
+	public Computer(String nom, Grid g, int difficulty) {
 		super(nom, g);
-		// TODO Auto-generated constructor stub
+		this.difficulty = difficulty;
 	}
 
 }
