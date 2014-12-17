206b04a698c9ca506b6f5143b4f9d730d9c9c416
rgdev <rgdev@users.noreply.github.com>
2014-12-17 18:44:26 +0100

    nettoyage et ajout de sprites

--- src/view/GridView.java
+++ src/view/GridView.java
@@ -14,45 +14,45 @@ import javax.swing.JLabel;
 import controller.GameController;
 
 public class GridView extends AbstractView implements Observer, MouseListener {
-	
+
 	private JLabel[][] cells;
 	private boolean isHuman;
 
 	/**
 	 * Create the panel.
 	 */
-	public GridView(GameController c) {
-		super(c);
+	public GridView() {
+		super(new GameController(null));
 		isHuman = false;
-		setLayout(new GridLayout(2, 2, 0, 0));
-		
+		setLayout(new GridLayout(2, 2, 1, 1));
+
 		JButton btnNewButton_1 = new JButton();
 		add(btnNewButton_1);
-		
+
 		JButton btnNewButton_2 = new JButton();
 		add(btnNewButton_2);
-		
+
 		JButton btnNewButton_3 = new JButton();
 		add(btnNewButton_3);
-		
+
 		JButton btnNewButton = new JButton();
 		add(btnNewButton);
-		
+
 	}
-	
+
 	public GridView(GameController c, int mapSize, boolean showShips, boolean isHuman) {
 		super(c);
 		this.isHuman = isHuman;
-		setLayout(new GridLayout(mapSize, mapSize, 0, 0));
+		setLayout(new GridLayout(mapSize, mapSize, 1, 1));
 		cells = new JLabel[mapSize][mapSize];
-		for(int i = 0; i<mapSize; i++)
+		for(int i = 0; i<mapSize; i++) {
 			for(int j = 0; j<mapSize; j++)
 			{
-				Image newimg = new ImageIcon("assets/v_5_5.png").getImage().getScaledInstance((int)(400/mapSize), (int)(400/mapSize),  java.awt.Image.SCALE_SMOOTH);  
+				Image newimg = new ImageIcon("assets/water.png").getImage().getScaledInstance(400/mapSize, 400/mapSize,  java.awt.Image.SCALE_SMOOTH);
 
 
 				JLabel btn = new JLabel( new ImageIcon(newimg) );
-				
+
 				/*ImageIcon water = new ImageIcon("assets/v_5_5.png");
 				btn.setIcon(water);
 				btn.setPreferredSize(new Dimension(50,50));*/
@@ -60,20 +60,21 @@ public class GridView extends AbstractView implements Observer, MouseListener {
 				btn.addMouseListener(this);
 				add(btn);
 			}
-		
-		Image newimg = new ImageIcon("assets/h_2_1.png").getImage().getScaledInstance((int)(400/mapSize), (int)(400/mapSize),  java.awt.Image.SCALE_SMOOTH);
+		}
+
+		Image newimg = new ImageIcon("assets/hit.png").getImage().getScaledInstance(400/mapSize, 400/mapSize,  java.awt.Image.SCALE_SMOOTH);
 		cells[0][0].setIcon(new ImageIcon(newimg));
-		
-		Image newimg2 = new ImageIcon("assets/h_2_2.png").getImage().getScaledInstance((int)(400/mapSize), (int)(400/mapSize),  java.awt.Image.SCALE_SMOOTH);
+
+		Image newimg2 = new ImageIcon("assets/h_2_2.png").getImage().getScaledInstance(400/mapSize, 400/mapSize,  java.awt.Image.SCALE_SMOOTH);
 		cells[0][1].setIcon(new ImageIcon(newimg2));
 	}
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		
-		
+
+
 	}
-	
+
 	public boolean isHuman()
 	{
 		return isHuman;
@@ -81,35 +82,35 @@ public class GridView extends AbstractView implements Observer, MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		if(!isHuman)
-			
-		// TODO Auto-generated method stub
-		System.out.println(arg0.getSource().toString());
-		
+		if(!isHuman) {
+			// TODO Auto-generated method stub
+			System.out.println(arg0.getSource().toString());
+		}
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
