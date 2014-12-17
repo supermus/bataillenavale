badfe36074a9e72f41b595e641fbe920a2fdfb10
rgdev <rgdev@users.noreply.github.com>
2014-12-17 18:52:02 +0100

    Position est maintenant Comparable

--- src/model/Position.java
+++ src/model/Position.java
@@ -1,9 +1,9 @@
 package model;
 
-public class Position {
-	
+public class Position implements Comparable<Position>{
+
 	private int x, y;
-	
+
 	public Position(int x, int y)
 	{
 		this.x = x;
@@ -12,7 +12,7 @@ public class Position {
 	}
 	public Position()
 	{
-		
+
 	}
 
 	public int getX() {
@@ -30,7 +30,7 @@ public class Position {
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Position [x=" + x + ", y=" + y + "]";
@@ -41,9 +41,18 @@ public class Position {
 		Position position=new Position(x, y);
 		return position;
 	}
-	
+
 	public boolean equals(Position p)
 	{
 		return (this.x == p.getX() && this.y == p.getY());
+	}
+
+	@Override
+	public int compareTo(Position pos) {
+		if(x == pos.getX() && y == pos.getY()) {
+			return 0;
+		} else {
+			return 1;
+		}
 	}
 }
